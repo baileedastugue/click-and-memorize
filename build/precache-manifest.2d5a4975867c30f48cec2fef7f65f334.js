38aaf04ed2e85e7d011cd48c60800653dfc51dc8
Bailee Dastugue <bailee.dast@gmail.com>
2020-06-27 10:08:12 -0500

    deleted node modules

---
+++ build/precache-manifest.2d5a4975867c30f48cec2fef7f65f334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f5c98be45236be8da737ce23acd8ff",
+    "url": "/click-and-memorize/index.html"
+  },
+  {
+    "revision": "d579a1df677a11df9c26",
+    "url": "/click-and-memorize/static/css/main.7ac3b58e.chunk.css"
+  },
+  {
+    "revision": "a4bacc115fc53ee552a6",
+    "url": "/click-and-memorize/static/js/2.023af906.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/click-and-memorize/static/js/2.023af906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d579a1df677a11df9c26",
+    "url": "/click-and-memorize/static/js/main.d766717f.chunk.js"
+  },
+  {
+    "revision": "9393e6425eaadb7df6f5",
+    "url": "/click-and-memorize/static/js/runtime-main.44cb6bd1.js"
+  }
+]);
